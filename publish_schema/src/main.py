5730ffc24a116db8cb1f6bbefcbbaa0991bc02b1
Farhang Tarlan <58827627+connected-ftarlan@users.noreply.github.com>
2022-03-17 11:47:03 -0400

    Branch test (#8)
    * testing pr merges
    
    * unit testing

--- publish_schema/src/main.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", dest="f", required=True, help="path to the json schema file to be uploaded")
-args = parser.parse_args()
-
-print(args.f)
---
+++ test/test_schema_parser.py
@@ -0,0 +1,4 @@
+from src.publish_schema.schema_parser import add
+
+def test_add():
+    assert add(1, 3) == 4
